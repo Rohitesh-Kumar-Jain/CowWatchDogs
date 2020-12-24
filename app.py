d9e90d6f00fcb26064e7d5c8005be0c923a17ab0
ROHITESH-KUMAR-JAIN <thekumarjain@gmail.com>
2020-12-24 19:09:56 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Welcome to our mission of saving Cows'
+
+
+if __name__ == '__main__':
+    app.run(debug= True)
