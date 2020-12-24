0bf87baaa96566c9e82e4f78160b9abe90953645
ROHITESH-KUMAR-JAIN <thekumarjain@gmail.com>
2020-12-24 22:27:04 +0530

    Made templates and added basic styling

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+@app.route('/welcome')
 @app.route('/')
 def hello_world():
-    return 'Welcome to our mission of saving Cows'
+    return render_template("welcome.html")
 
 
 if __name__ == '__main__':
-    app.run(debug= True)
+    app.run('0.0.0.0', 8085,debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
+    # Use $ export FLASK_DEBUG=1   for not rerunning it again and again.
