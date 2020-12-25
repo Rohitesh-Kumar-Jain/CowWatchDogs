a1861592e676e348d24ad241d891f5374abea1ab
ROHITESH-KUMAR-JAIN <thekumarjain@gmail.com>
2020-12-25 09:47:43 +0530

    First Commit

--- app.py
+++ app.py
@@ -11,3 +11,4 @@ def hello_world():
 if __name__ == '__main__':
     app.run('0.0.0.0', 8085,debug=True, use_debugger=False, use_reloader=False, passthrough_errors=True)
     # Use $ export FLASK_DEBUG=1   for not rerunning it again and again.
+    #flask run --port 8085     to run in debug mode.
